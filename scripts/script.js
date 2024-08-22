e0e1a1e70da5e1e76d19e932103cffe27d857954
Ryan Choi <86131414+gkh22@users.noreply.github.com>
2024-08-21 17:53:32 -0700

    Implement logic and images

--- scripts/script.js
+++ scripts/script.js
@@ -12,94 +12,92 @@ function getComputerChoice() {
     }
 }
 
-
-
-function isValidInput(input) {
-    input = input.toLowerCase();
-    if (input != "rock" && input != "scissors" && input != "paper") {
-        return false;
-    }
-    return true;
-}
-
-function getHumanChoice() {
-    let humanChoice = "";
-    do {
-        humanChoice = prompt("Rock, Paper, Scissors?: ");
-    } while (!isValidInput(humanChoice));
-    return humanChoice;
-}
-
 function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
     result = -1;
-
     if (humanChoice == "rock") {
         if (computerChoice == "scissors") {
-            
             result = 1;
         }
         else if (computerChoice == "paper") {
-            
             result = 0;
         }
     }
     else if (humanChoice == "paper") {
         if (computerChoice == "rock") {
-            
             result = 1;
         }
         else if (computerChoice == "scissors") {
-            
             result = 0;
         }
     }
     else {
         if (computerChoice == "paper") {
-            
             result = 1;
         }
         else if (computerChoice == "rock") {
-        
             result = 0;
         }
     }
-    return result;
+    calculateScores(result);
+    displayResult(result);
 }
 
-function playGame(rounds) {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < rounds; i++) {
-        result = playRound(getHumanChoice(), getComputerChoice());
-        if (result == 1) {
-            humanScore += 1;
-            console.log("You win!");
-        }
-        else if (result == 0) {
-            computerScore += 1;
-            console.log("You lose!");
-        }
-        else {
-            console.log("Tie!");
-        }
+function displayResult(result) {
+    let human_counter = document.querySelector(".human_score");
+    let computer_counter = document.querySelector(".computer_score");
+    let restart = document.querySelector(".restart");
+    let buttons = document.querySelector(".buttons");
+    if (human_score == 5) {
+        human_counter.textContent = "Radical, you won!";
+        computer_counter.textContent = "Beep, borrrpp..";
+        buttons.classList.toggle("invisible");
+        restart.classList.toggle("invisible");
     }
-
-    if (humanScore > computerScore) {
-        console.log("Human victory!");
-    }
-    else if (computerScore > humanScore) {
-        console.log("Computer victory..");
+    else if (computer_score == 5) {
+        human_counter.textContent = "You lost!";
+        computer_counter.textContent = "Boop! Boop Beep!";
+        buttons.classList.toggle("invisible");
+        restart.classList.toggle("invisible");
     }
     else {
-        console.log("Tie!");
+        human_counter.textContent = human_score;
+        computer_counter.textContent = computer_score;
     }
 }
 
-playGame(5);
+function calculateScores(result) {
+    if (result == 1) {
+        human_score += 1;
+    }
+    else if (result == 0) {
+        computer_score += 1;
+    }
+}
 
+function resetGame() {
+    let human_counter = document.querySelector(".human_score");
+    let computer_counter = document.querySelector(".computer_score");
+    let restart = document.querySelector(".restart");
+    let buttons = document.querySelector(".buttons");
 
+    human_score = 0;
+    computer_score = 0;
 
+    human_counter.textContent = 0;
+    computer_counter.textContent = 0;
+    buttons.classList.remove("invisible");
+    restart.classList.add("invisible");
+}
 
+resetGame();
+
+let rock_button = document.querySelector(".rock");
+let scissors_button = document.querySelector(".scissors");
+let paper_button = document.querySelector(".paper");
+let play_again = document.querySelector(".play_again");
+
+rock_button.addEventListener("click", () => { playRound("rock", getComputerChoice()); });
+paper_button.addEventListener("click", () => { playRound("paper", getComputerChoice()); });
+scissors_button.addEventListener("click", () => { playRound("scissors", getComputerChoice()); });
+play_again.addEventListener("click", resetGame);
 
